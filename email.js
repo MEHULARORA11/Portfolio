8288d9d01f4f32e5d508eddb072361d0939b4318
MEHULARORA11 <mehularora505@gmail.com>
2026-06-05 00:02:26 +0530

    fix:change config of nodemailer forcing it to use Ipv4

--- email.js
+++ email.js
@@ -4,15 +4,20 @@ import 'dotenv/config'
 // SMTP transporter — works with Mailtrap, Gmail, SendGrid, or any SMTP provider
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT) || 587,
+  port: Number(process.env.SMTP_PORT),
   secure: Number(process.env.SMTP_PORT) === 465,
-   family: 4,
+
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
-});
 
+  family: 4,
+
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+});
 transporter.verify((error, success) => {
   if (error) {
     console.log("SMTP ERROR =>", error);
